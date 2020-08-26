f3d85539b7b42082e390433ec58e4dad8d91d64d
Giorgos Stamatelatos <gstamatelat@gmail.com>
2020-08-26 12:14:59 +0300

    Heavily refactor PartitionsIterator

--- src/main/java/gr/james/partition/PartitionsIterator.java
+++ src/main/java/gr/james/partition/PartitionsIterator.java
@@ -1,40 +1,33 @@
 package gr.james.partition;
 
 class PartitionsIterator {
-    final int[] indices;
-    final int[] m;
-    final int base;
+    final int[] a;
+    final int[] b;
+    final int n;
     boolean isFirst = true;
 
     public PartitionsIterator(int numberOfElements) {
         assert numberOfElements > 0;
-        this.base = numberOfElements;
-        this.indices = new int[this.base];
-        this.m = new int[this.base];
+        this.n = numberOfElements;
+        this.a = new int[this.n];
+        this.b = new int[this.n];
     }
 
     public int[] next() {
         if (isFirst) {
             isFirst = false;
-            return this.indices;
+            return a;
         }
-        boolean changed = false;
-        int i;
-        for (i = indices.length - 1; i > 0; i--) {
-            if (indices[i] < this.base - 1 && indices[i] <= m[i]) {
-                indices[i]++;
-                changed = true;
-                break;
-            }
+        int i = n - 1;
+        while (a[i] == n - 1 || a[i] > b[i]) {
+            i--;
         }
-        for (i = i + 1; i < this.base; i++) {
-            indices[i] = 0;
-            m[i] = Math.max(m[i - 1], indices[i - 1]);
+        if (i == 0) return null;
+        a[i]++;
+        for (i = i + 1; i < n; i++) {
+            a[i] = 0;
+            b[i] = Math.max(b[i - 1], a[i - 1]);
         }
-        if (changed) {
-            return this.indices;
-        } else {
-            return null;
-        }
+        return a;
     }
 }
