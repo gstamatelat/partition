f53be43b27c5615560949fef860c42cf205188e9
Giorgos Stamatelatos <gstamatelat@gmail.com>
2020-08-27 15:17:38 +0300

    Simplify PartitionsIteratorBetweenK

--- src/main/java/gr/james/partition/PartitionsIteratorBetweenK.java
+++ src/main/java/gr/james/partition/PartitionsIteratorBetweenK.java
@@ -19,7 +19,7 @@ class PartitionsIteratorBetweenK {
         this.kmin = kmin;
         this.kmax = kmax;
         this.a = new int[this.n];
-        this.b = new int[this.n];
+        this.b = new int[this.n + 1];
         for (int i = this.n - 1; i > this.n - this.kmin; i--) {
             a[i] = this.kmin - this.n + i;
             b[i] = this.kmin - this.n + i - 1;
@@ -27,25 +27,35 @@ class PartitionsIteratorBetweenK {
     }
 
     public int[] next() {
+        /* Return the first element */
         if (isFirst) {
             isFirst = false;
             return a;
         }
+
+        /* Find which index to increment going backwards */
         int i = n - 1;
         while (a[i] == kmax - 1 || a[i] > b[i]) {
             i--;
         }
         if (i == 0) return null;
         a[i]++;
-        int zeroes = Math.max(a[i], b[i]) + n - i - kmin;
+
+        /* Start going forward putting zeroes for as long as we can */
+        b[i + 1] = Math.max(a[i], b[i]);
+        int zeroes = b[i + 1] + n - i - kmin;
         for (i = i + 1; zeroes > 0 && i < n; i++, zeroes--) {
             a[i] = 0;
-            b[i] = Math.max(b[i - 1], a[i - 1]);
+            b[i + 1] = b[i];
         }
+
+        /* Keep going forward putting the minimum values that we can */
         for (; i < n; i++) {
-            a[i] = Math.max(b[i - 1], a[i - 1]) + 1;
-            b[i] = Math.max(b[i - 1], a[i - 1]);
+            a[i] = b[i] + 1;
+            b[i + 1] = a[i];
         }
+
+        /* Return */
         return a;
     }
 }
