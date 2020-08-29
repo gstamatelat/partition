0d48098f1f899e59cddfcea88f5b35a362cc815f
Giorgos Stamatelatos <gstamatelat@gmail.com>
2020-08-29 18:10:36 +0300

    Implement reverseLexicographicEnumeration (#9)

---
+++ src/main/java/gr/james/partition/ReversePartitionsIterator.java
@@ -0,0 +1,74 @@
+package gr.james.partition;
+
+class ReversePartitionsIterator {
+    final int[] a;
+    final int[] b;
+    final int n;
+    final int kmin;
+    final int kmax;
+    boolean isFirst = true;
+
+    public ReversePartitionsIterator(int n, int kmin, int kmax) {
+        assert n > 0;
+        assert kmin > 0;
+        assert kmax > 0;
+        assert n >= kmin;
+        assert n >= kmax;
+        assert kmax >= kmin;
+        this.n = n;
+        this.kmin = kmin;
+        this.kmax = kmax;
+        this.a = new int[this.n];
+        this.b = new int[this.n + 1];
+        for (int i = 0; i < this.n; i++) {
+            a[i] = Math.min(i, kmax - 1);
+            b[i + 1] = a[i];
+        }
+    }
+
+    private boolean validate() {
+        int max = 0;
+        assert a[0] == 0;
+        for (int i = 0; i < n; i++) {
+            assert a[i] <= kmax - 1;
+            assert a[i] <= max + 1;
+            max = Math.max(a[i], max);
+        }
+        assert max >= kmin - 1;
+        return true;
+    }
+
+    public int[] next() {
+        /* Return the first element */
+        if (isFirst) {
+            isFirst = false;
+            return a;
+        }
+
+        /* Move backwards and find which index to decrement */
+        int i = n - 1;
+        while (i > 0 && (a[i] == 0 || kmin - b[i] > n - i)) {
+            i = i - 1;
+        }
+        if (i == 0) return null;
+        a[i] = a[i] - 1;
+        b[i + 1] = Math.max(a[i], b[i]);
+
+        /* Switch direction and place the maximum values possible */
+        for (i = i + 1; b[i] < kmax - 1 && i < n; i++) {
+            a[i] = b[i] + 1;
+            b[i + 1] = a[i];
+        }
+
+        /* Maintain direction and fill the remaining places with kmin-1 */
+        for (; i < n; i++) {
+            a[i] = kmax - 1;
+            b[i + 1] = kmax - 1;
+        }
+
+        assert validate();
+
+        /* Return */
+        return a;
+    }
+}
---
+++ src/test/java/gr/james/partition/ReversePartitionsIteratorTests.java
@@ -0,0 +1,142 @@
+package gr.james.partition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.function.BiFunction;
+
+/**
+ * Test for the {@link Partitions#reverseLexicographicEnumeration(Set, int, int, BiFunction)} method, and consequently
+ * the {@link ReversePartitionsIterator} class.
+ */
+public class ReversePartitionsIteratorTests {
+    /**
+     * The number of possible partitions of 10 elements with kmin=4 and kmax=6 should be exactly 99457.
+     */
+    @Test
+    public void correctness1() {
+        final Iterator<Partition<Integer>> it = Partitions.reverseLexicographicEnumeration(Helper.newHashSet(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), 4, 6, ImmutablePartition::new);
+        final Set<Partition<Integer>> partitions = new HashSet<>();
+        int count = 0;
+        while (it.hasNext()) {
+            final Partition<Integer> p = it.next();
+            Assert.assertEquals(10, p.size());
+            partitions.add(p);
+            count++;
+        }
+        Assert.assertEquals(99457, partitions.size());
+        Assert.assertEquals(99457, count);
+    }
+
+    /**
+     * The number of possible partitions of 10 elements with kmin=5 and kmax=5 should be exactly 42525.
+     */
+    @Test
+    public void correctness2() {
+        final Iterator<Partition<Integer>> it = Partitions.reverseLexicographicEnumeration(Helper.newHashSet(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), 5, 5, ImmutablePartition::new);
+        final Set<Partition<Integer>> partitions = new HashSet<>();
+        int count = 0;
+        while (it.hasNext()) {
+            final Partition<Integer> p = it.next();
+            Assert.assertEquals(10, p.size());
+            partitions.add(p);
+            count++;
+        }
+        Assert.assertEquals(42525, partitions.size());
+        Assert.assertEquals(42525, count);
+    }
+
+    /**
+     * The number of possible partitions of 10 elements with kmin=1 and kmax=5 should be exactly 86472.
+     */
+    @Test
+    public void correctness3() {
+        final Iterator<Partition<Integer>> it = Partitions.reverseLexicographicEnumeration(Helper.newHashSet(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), 1, 5, ImmutablePartition::new);
+        final Set<Partition<Integer>> partitions = new HashSet<>();
+        int count = 0;
+        while (it.hasNext()) {
+            final Partition<Integer> p = it.next();
+            Assert.assertEquals(10, p.size());
+            partitions.add(p);
+            count++;
+        }
+        Assert.assertEquals(86472, partitions.size());
+        Assert.assertEquals(86472, count);
+    }
+
+    /**
+     * The method should throw {@link NullPointerException} if the first input is {@code null}.
+     */
+    @Test(expected = NullPointerException.class)
+    public void nullInput1() {
+        final Set<Integer> s = null;
+        Partitions.reverseLexicographicEnumeration(s, 1, 1, (elements, mapping) -> new ImmutablePartition<>(new UnionFindPartition<>()));
+    }
+
+    /**
+     * The method should throw {@link NullPointerException} if the second input is {@code null}.
+     */
+    @Test(expected = NullPointerException.class)
+    public void nullInput2() {
+        final Set<Integer> s = new HashSet<>();
+        Partitions.reverseLexicographicEnumeration(s, 1, 1, null);
+    }
+
+    /**
+     * The method should throw {@link NullPointerException} if any element in {@code elements} is {@code null}.
+     */
+    @Test(expected = NullPointerException.class)
+    public void nullInput3() {
+        final Set<Integer> s = Helper.newHashSet(1, 2, null);
+        Partitions.reverseLexicographicEnumeration(s, 1, 1, (elements, mapping) -> new ImmutablePartition<>(new UnionFindPartition<>()));
+    }
+
+    /**
+     * The method should throw {@link IllegalArgumentException} if {@code elements} is empty.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void emptyOfSet() {
+        final Set<Integer> s = new HashSet<>();
+        Partitions.reverseLexicographicEnumeration(s, 1, 1, (elements, mapping) -> new ImmutablePartition<>(new UnionFindPartition<>()));
+    }
+
+    /**
+     * The method should throw {@link IllegalArgumentException} if kmin < 1.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void notPositiveKmin() {
+        final Set<Integer> s = new HashSet<>(Arrays.asList(1, 2, 3));
+        Partitions.reverseLexicographicEnumeration(s, 0, 1, (elements, mapping) -> new ImmutablePartition<>(new UnionFindPartition<>()));
+    }
+
+    /**
+     * The method should throw {@link IllegalArgumentException} if kmax < 1.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void notPositiveKmax() {
+        final Set<Integer> s = new HashSet<>(Arrays.asList(1, 2, 3));
+        Partitions.reverseLexicographicEnumeration(s, 0, 0, (elements, mapping) -> new ImmutablePartition<>(new UnionFindPartition<>()));
+    }
+
+    /**
+     * The method should throw {@link IllegalArgumentException} is kmax is greater than the size of elements.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void kBiggerThanN() {
+        final Set<Integer> s = new HashSet<>(Arrays.asList(1, 2, 3));
+        Partitions.reverseLexicographicEnumeration(s, 2, 4, (elements, mapping) -> new ImmutablePartition<>(new UnionFindPartition<>()));
+    }
+
+    /**
+     * The method should throw {@link IllegalArgumentException} is kmin is greater than kmax.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void kMinGreaterThanKmax() {
+        final Set<Integer> s = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5));
+        Partitions.reverseLexicographicEnumeration(s, 4, 3, (elements, mapping) -> new ImmutablePartition<>(new UnionFindPartition<>()));
+    }
+}
